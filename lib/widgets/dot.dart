665b6a3bdc9981e8ab5739b4a5b6692621272ff2
Abhishek <abhishekverma7t@gmail.com>
2025-02-03 00:57:14 +0530

    minor changes

--- lib/widgets/dot.dart
+++ lib/widgets/dot.dart
@@ -1,20 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:left/core/palette.dart';
 
+enum DotSize {
+  month,
+  year,
+  life,
+}
+
 class Dot extends StatelessWidget {
   const Dot({
     super.key,
     this.dull = false,
+    this.dotSize = DotSize.month,
   });
 
   final bool dull;
+  final DotSize dotSize;
 
   @override
   Widget build(BuildContext context) {
+    double size;
+
+    switch (dotSize) {
+      case DotSize.month:
+        size = 16.0;
+        break;
+      case DotSize.year:
+        size = 14.0;
+        break;
+      case DotSize.life:
+        size = 10.0;
+        break;
+    }
+
     return CircleAvatar(
       backgroundColor:
           dull ? AppPalette.secondary.withAlpha(69) : AppPalette.secondary,
-      radius: 16.0,
+      radius: size,
     );
   }
 }
